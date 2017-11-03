079275d884c0f60b7582204c88988c0b5497434a
anuding <anuding@126.com>
2017-11-03 21:52:20 +0800

    RADIX加了注释，未完

--- CODE_BANK/排序算法/Radixsort/Radixsort/main.cpp
+++ CODE_BANK/排序算法/Radixsort/Radixsort/main.cpp
@@ -1,7 +1,7 @@
 
 /*
 * -----------------------------------------------------------
-*  Copyright (c) 2014 anuding All rights reserved.
+*  Copyright (c) 2017 anuding All rights reserved.
 * -----------------------------------------------------------
 *
 *        创建者：  anuding
@@ -10,8 +10,6 @@
 *
 */
 
-
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -19,41 +17,27 @@
 #include<time.h>
 #include<stdio.h>
 #include<stdlib.h>
+#define N 10000
+static long long int swaptimes = 0;
+static long long int comparetimes = 0;
+static double totaltime;
+static clock_t start, finish;
+
 using namespace std;
-#define N 25
-
-template <typename E, typename getKey>
-void radix(E A[], E B[],
-	int n, int k, int r, int cnt[]) {
-	// cnt[i] stores number of records in bin[i]
-	int j;
-	for (int i = 0, rtoi = 1; i<k; i++, rtoi *= r) { // For k digits
-		for (j = 0; j<r; j++) cnt[j] = 0; // Initialize cnt
-										  // Count the number of records for each bin on this pass
-		for (j = 0; j<n; j++) cnt[(getKey::key(A[j]) / rtoi) % r]++;
-		// Index B: cnt[j] will be index for last slot of bin j.
-		for (j = 1; j<r; j++) cnt[j] = cnt[j - 1] + cnt[j];
-		// Put records into bins, work from bottom of each bin.
-		// Since bins fill from bottom, j counts downwards
-		for (j = n - 1; j >= 0; j--)
-			B[--cnt[(getKey::key(A[j]) / rtoi) % r]] = A[j];
-		for (j = 0; j<n; j++) A[j] = B[j]; // Copy B back to A
-	}
-}
-
 
 template <typename E>
 void radixsort(E A[]) 
 {
-	int box[10][N];
-	int flag[10] = {0};
+	int box[10][N];//box用来存储每种余数的数据，box[4][0]表示余数为4的第一个数据
+	int flag[10] = {0};//flag表示每种余数的数据总量，flag[4]==5表示共有5个余数为4的数据
+	int bit = N / 10;
 	for (int i = 0; i<N; i++)
 	{
 		int temp = A[i] % 10;
 		box[temp][flag[temp]] = A[i];
 		flag[temp]++;
 	}
-	//flag[10] = { 0 };
+	
 	int num=0;
 	for(int k=0;k<10;k++)
 		for (int i = 0; i < 10; i++)
@@ -74,39 +58,46 @@ void radixsort(E A[])
 	
 }
 
+
 int main()
 {
+	void printresult();
+
 	int *ori, *temp;
 	ori = new int[N];//存放在堆上
-	temp = new int[N];
-
 	srand((unsigned)time(NULL)); //用当前系统时间设置种子
 
+
+								 //平均情况
 	for (int i = 0; i < N; i++)
 	{
 		int k = rand() % (N + 1);
 		ori[i] = k;
+
 	}
 	for (int i = 0; i < N; i++)
-	{
 		cout << ori[i] << " ";
-	}
-	cout << endl;
-	clock_t start, finish;
-	double totaltime;
 	start = clock();
-
 	radixsort<int>(ori);
-
 	finish = clock();
-	totaltime = (double)(finish - start) / CLOCKS_PER_SEC;
-	cout << "\n此程序的运行时间为" << totaltime << "秒！" << endl;
+	cout << "平均情况下:" << endl;
+	printresult();
 	cout << endl;
-	
+
 	for (int i = 0; i < N; i++)
-	{
-		cout<<ori[i]<<" ";
-	}
-    return 0;
+		cout << ori[i] << " ";
+	return 0;
 }
 
+
+
+void printresult()
+{
+	totaltime = (double)(finish - start) / CLOCKS_PER_SEC;
+	cout << "此程序的运行时间为" << totaltime << "秒！" << endl;
+	cout << "共比较" << comparetimes << "次" << endl;
+	cout << "共交换" << swaptimes << "次" << endl;
+	swaptimes = 0;
+	comparetimes = 0;
+	cout << endl;
+}
